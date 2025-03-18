10c893db9026bf4bd70d22af784bfc46fe4e961d
Alex Schouls <alex_schouls@live.com>
2025-03-18 11:38:59 +0100

    added extra page to view data

---
+++ src/CosmosPagination.Web/Pages/Data.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CosmosPagination.Web.Pages;
+
+public class DataModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
