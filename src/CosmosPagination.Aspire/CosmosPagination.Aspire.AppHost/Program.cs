a1fabcf385937d9b6245077d26243b7d8a62b133
Alex Schouls <alex_schouls@live.com>
2025-03-24 12:04:10 +0100

    using regular emulator, not preview

--- src/CosmosPagination.Aspire/CosmosPagination.Aspire.AppHost/Program.cs
+++ src/CosmosPagination.Aspire/CosmosPagination.Aspire.AppHost/Program.cs
@@ -1,8 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-#pragma warning disable ASPIRECOSMOSDB001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-var cosmos = builder.AddAzureCosmosDB("cosmos").RunAsPreviewEmulator(emulator => emulator.WithDataExplorer());
-#pragma warning restore ASPIRECOSMOSDB001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+var cosmos = builder.AddAzureCosmosDB("cosmos").RunAsEmulator();
 
 builder.AddProject<Projects.CosmosPagination_Web>("web").WithReference(cosmos);
 
